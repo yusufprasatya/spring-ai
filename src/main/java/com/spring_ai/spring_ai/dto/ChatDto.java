cc1c0802fc2198b52dac4c4b1efb4175eb0c9d81
yusufprasatya <yusufmarsudien10@gmail.com>
2025-02-18 00:33:51 +0700

    update workflow

--- src/main/java/com/spring_ai/spring_ai/dto/ChatDto.java
+++ src/main/java/com/spring_ai/spring_ai/dto/ChatDto.java
@@ -3,7 +3,8 @@ package com.spring_ai.spring_ai.dto;
 public class ChatDto {
     private String message;
 
-    public String getMessage() {
-        return message;
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
